993e7eede0ce437f9d6bf2749daad3465ae61d64
natalie-todd <natalie.todd@hashicorp.com>
2023-02-24 12:24:16 -0700

    Add README.md and main.tf

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  cloud {
+    workspaces {
+      name = "agent-oasis-test"
+    }
+    organization = "ntodd"
+  }
+
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+variable "username" {
+  type = string
+  default = "last_name"
+}
+
+resource "null_resource" "random" {
+  triggers = {
+    username = var.username
+  }
+}
+
+resource "random_pet" "always_new" {
+  keepers = {
+    uuid = uuid()
+  }
+  length = 4
+}
+
+output "username" {
+  value = "Username is ${var.username}"
+}
+
+output "random" {
+  #  all resources that are invoked as variables have an accessible id attribute
+  value = "Changed to ${null_resource.random.id}"
+}
+
+output "pet" {
+  value = { name_of_pet : random_pet.always_new.id }
+}
+
+output "pet_length" {
+  value = "Length is ${random_pet.always_new.length}"
+}
